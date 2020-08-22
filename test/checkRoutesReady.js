70340f6c389c9512dc8cc752d54ac7e35b3a4948
“mike14747” <mike4747@oh.rr.com>
2020-08-22 11:02:05 -0400

    Added a basic routesReady and integration test. Changed config/users.json to make the name key an object instead of an array.

---
+++ test/checkRoutesReady.js
@@ -0,0 +1,26 @@
+const server = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.should();
+chai.use(chaiHttp);
+
+describe('Test API routes', function () {
+    const runTests = () => {
+        require('./apiTests/usersAPI');
+    };
+
+    const checkRoutes = () => {
+        it('should check and see if the API routes are ready', function (done) {
+            chai.request(server)
+                .get('/api/test')
+                .then(response => {
+                    if (response.status === 200) runTests();
+                    response.should.have.status(200);
+                    done();
+                })
+                .catch(error => done(error));
+        });
+    };
+
+    checkRoutes();
+});
