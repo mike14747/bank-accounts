24dc02ab0d4256652f557ff879bf525d7762ec1e
“mike14747” <mike4747@oh.rr.com>
2020-09-13 22:59:15 -0400

    Made some progress on joining the foreign key ids in the accounts collection model.

--- models/accounts.js
+++ models/accounts.js
@@ -4,7 +4,31 @@ const ObjectID = require('mongodb').ObjectID;
 const Account = {
     getAllAccounts: async () => {
         try {
-            const result = await db.collection('accounts').find({}).sort({ _id: 1 }).toArray();
+            // const result = await db.collection('accounts').find({}).sort({ _id: 1 }).toArray();
+            const result = await db.collection('accounts').aggregate([
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'user_id',
+                        foreignField: '_id',
+                        as: 'userInfo',
+                    },
+                },
+                {
+                    $unwind: '$userInfo',
+                },
+                {
+                    $lookup: {
+                        from: 'account_types',
+                        localField: 'account_type',
+                        foreignField: '_id',
+                        as: 'accountTypeInfo',
+                    },
+                },
+                {
+                    $unwind: '$accountTypeInfo',
+                },
+            ]).project({ user_id: 0, account_type: 0, 'userInfo.password': 0, 'accountTypeInfo._id': 0 }).toArray();
             return [result, null];
         } catch (error) {
             return [null, error];
@@ -60,3 +84,15 @@ const Account = {
 };
 
 module.exports = Account;
+
+// db.collection('accounts').aggregate([
+//     {
+//         $lookup:
+//         {
+//             from: 'users',
+//             localField: 'user_id',
+//             foreignField: '_id',
+//             as: 'userInfo',
+//         },
+//     },
+// ]).toArray();
