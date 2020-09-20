fbff5a294e374539ed82e0574ce3701ccec0a123
“mike14747” <mike4747@oh.rr.com>
2020-09-19 22:18:48 -0400

    Got the connect-mongo session store working.

--- passport/sessionStore.js
+++ passport/sessionStore.js
@@ -1,24 +1,11 @@
-const pool = require('../config/connectionPool').getDb();
-
 const session = require('express-session');
-
-// change this to "connect-mongo"
-// const MySQLStore = require('express-mysql-session')(session);
 const MongoStore = require('connect-mongo')(session);
 
 const sessionOptions = {
-    // old options
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PW,
-    // database: process.env.DB_NAME,
-    // new options
-    clientPromise: pool,
+    url: process.env.MONGODB_URI,
     touchAfter: 24 * 3600,
 };
 
-// const sessionStore = new MongoStore(sessionOptions, pool);
 const sessionStore = new MongoStore(sessionOptions);
 
 module.exports = sessionStore;
--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(require('./controllers/testController'));
 
 mongodbConnect()
     .then(() => {
-        // app.use(require('./passport/expressSession'));
-        // const passport = require('./passport/passportFunctions');
-        // app.use(passport.initialize());
-        // app.use(passport.session());
+        app.use(require('./passport/expressSession'));
+        const passport = require('./passport/passportFunctions');
+        app.use(passport.initialize());
+        app.use(passport.session());
         app.use('/api', require('./controllers'));
     })
     .catch((error) => {
