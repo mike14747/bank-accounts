a263065a571a7cfb702d9bc9d5599ea8f4f04756
mike14747 <mike4747@oh.rr.com>
2020-09-21 13:11:35 -0400

    Created a few preliminary pages (login, account, dashboard and noMatch), then added them to routes in App.js. They don't have any content yet... just some text.

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,26 @@ import './css/my_style.css';
 import './css/styles.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import Login from './pages/login/login';
+import Dashboard from './pages/dashboard/dashboard';
+import Account from './pages/account/account';
+import NoMatch from './pages/noMatch/noMatch';
 
 function App() {
     return (
         <Router>
             <Header />
-            <Switch>
-                <Route exact path="/">
-                    <div className="container flex-fill bg-white border-left border-right border-dark">
+            <div className="container flex-fill bg-white border-left border-right border-dark">
+                <Switch>
+                    <Route exact path="/">
                         <h1>Starter code for body in App.js</h1>
-                    </div>
-                </Route>
-            </Switch>
+                    </Route>
+                    <Route exact path="/login"><Login /></Route>
+                    <Route exact path="/dashboard"><Dashboard /></Route>
+                    <Route exact path="/account"><Account /></Route>
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
             <Footer />
         </Router>
     );
---
+++ client/src/pages/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            Username: <input></input>
+            Password: <input></input>
+            <button>Submit</button>
+        </div>
+    );
+};
+
+export default Login;
